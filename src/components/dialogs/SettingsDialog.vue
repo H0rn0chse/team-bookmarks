c385b48189f62a676e0840c4ee30d55c54ea2c22
H0rn0chse <h0rn0chse@gmx.de>
2022-12-22 11:34:11 +0100

    Added about entry

--- src/components/dialogs/SettingsDialog.vue
+++ src/components/dialogs/SettingsDialog.vue
@@ -2,45 +2,54 @@
 import { reactive } from "vue";
 import { useDialogStore } from "@/stores/dialog";
 import TagsEditor from "@/components/settingsEntries/TagsEditorEntry.vue";
+import AboutEntry from "../settingsEntries/AboutEntry.vue";
 
 const dialogStore = useDialogStore();
 const data = reactive({
-  tab: "tags"
+  tab: "tagEditor"
 });
 </script>
 
 <template>
   <v-dialog
     v-model="dialogStore.settings.show"
-    min-width="350"
+    min-width="350" 
     max-width="1200"
     transition="dialog-top-transition"
   >
     <v-card>
-      <v-toolbar
-        color="primary"
-      >
-        <v-toolbar-title>Settings</v-toolbar-title>
-      </v-toolbar>
+      <v-card-title>Settings</v-card-title>
       <div class="d-flex flex-row">
         <v-tabs
           v-model="data.tab"
           direction="vertical"
           color="primary"
         >
-          <v-tab value="tags">
-            <v-icon start>
-              mdi-tab
-            </v-icon>
-            Tags
+          <v-tab value="tagEditor">
+            <vue-feather
+              type="tag"
+              class="settingsEntryIcon"
+            />
+            Tag Editor
+          </v-tab>
+          <v-tab value="about">
+            <vue-feather
+              type="coffee"
+              class="settingsEntryIcon"
+            />
+            About
           </v-tab>
         </v-tabs>
         <v-window
           v-model="data.tab"
-          class="tags"
+          class="settingsWindow"
         >
-          <v-window-item value="tags" />
-          <TagsEditor />
+          <v-window-item value="tagEditor">
+            <TagsEditor />
+          </v-window-item>
+          <v-window-item value="about">
+            <AboutEntry />
+          </v-window-item>
         </v-window>
       </div>
       <v-card-actions class="d-flex justify-end">
@@ -56,7 +65,11 @@ const data = reactive({
 </template>
 
 <style scoped>
-.tags {
-  width: 100%
+.settingsWindow {
+  width: 100%;
+}
+
+.settingsEntryIcon {
+  margin-right: 0.2em;
 }
 </style>
---
+++ src/components/settingsEntries/AboutEntry.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { } from "vue";
+</script>
+
+<template>
+  <div class="content">
+    About Entry
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 10em;
+}
+</style>
